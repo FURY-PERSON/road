8a04ee8f85032c87b2ff536eed77d0459ad1828d
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-02-04 20:43:04 +0300

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { Test } from './components/Test';
+import { Suspense } from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import "./styles.css"
 
 function App() {
   return (
-    <div>
-      <Test />
-    </div>
+    <BrowserRouter>
+        <Link to="/">To Main</Link>
+        <Link to="/about">To About</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path={'/'} element={<MainPageAsync />}/>
+            <Route path={'/about'} element={<AboutPageAsync />}/>
+          </Routes>
+        </Suspense>
+    </BrowserRouter>
   );
 }
 
