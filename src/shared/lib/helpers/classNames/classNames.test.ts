9d9f536b69dca6930ca6709575a7d8724480cffc
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-02-21 23:44:15 +0300

    add jest ts

--- src/shared/lib/helpers/classNames/classNames.test.ts
+++ src/shared/lib/helpers/classNames/classNames.test.ts
@@ -1,5 +1,27 @@
+import { classNames } from "./classNames";
+
 describe('classNames', () => {
-  test('test', () => {
-    expect(true).toBe(true)
+  test('only first param', () => {
+    expect(classNames('class')).toBe('class')
+  })
+
+  test('with additional class', () => {
+    expect(classNames('class', {} , ['class2'])).toBe('class class2')
+  })
+
+  test('with additional class', () => {
+    expect(classNames('class', {} , ['class2', 'class3'])).toBe('class class2 class3')
+  })
+
+  test('with mod', () => {
+    expect(classNames('class', {class2: true})).toBe('class class2')
+  })
+
+  test('with many mods', () => {
+    expect(classNames('class', {class3: false, class2: true})).toBe('class class2')
+  })
+
+  test('with mods and additional class', () => {
+    expect(classNames('class', {class2: true}, ['class3'])).toBe('class class3 class2')
   })
 });
