554fd4aee240767ee342567bbf4874ff812dd393
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-02-23 00:01:13 +0300

    add jest-dom test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,6 +34,17 @@ export default {
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
   rootDir: '../../',
+  modulePaths: [
+    '<rootDir>src',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTest.ts'],
+
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -3,20 +3,28 @@ import { useTheme } from 'shared/contexts/ThemeProvider';
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import cls from './Button.module.scss';
 
+export enum ThemeVariant {
+  CLEAR = 'clear'
+}
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   variant?: ThemeVariant
 }
 
 export const Button:FC<ButtonProps> = memo((props) => {
- const { className, children, variant = ThemeVariant.CLEAR, ...otherProps } = props;
- const {theme} = useTheme();
+  const {
+    className, children, variant = ThemeVariant.CLEAR, ...otherProps 
+  } = props;
+  const { theme } = useTheme();
 
- return (
-    <button {...otherProps} className={classNames(cls.Button, {}, [className, cls[variant]])}>{children}</button>
- );
-})
-
-export enum ThemeVariant {
-  CLEAR = 'clear'
-}
+  return (
+    <button 
+      {...otherProps} 
+      type="button" 
+      className={classNames(cls.Button, {}, [className, cls[variant]])}
+    >
+      {children}
+    </button>
+  );
+});
