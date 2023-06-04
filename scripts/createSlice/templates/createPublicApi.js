e47169799023bc4341a88c3a85c9be996fe3f26e
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2023-06-04 10:03:19 +0300

    add fsd slices generator

---
+++ scripts/createSlice/templates/createPublicApi.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs/promises');
+const resolveRoot = require('../resolveRoot');
+const firstCharUpperCase = require('../firstCharUpperCase');
+
+module.exports = async (layer, sliceName) => {
+  const componentName = firstCharUpperCase(sliceName);
+
+  try {
+    await fs.writeFile(
+      resolveRoot('src', layer, sliceName, 'index.ts'),
+      `export { ${componentName} } from './ui/${componentName}/${componentName}';`,
+    );
+  } catch (e) {
+    console.log('Не удалось создать PUBLIC API');
+  }
+};
