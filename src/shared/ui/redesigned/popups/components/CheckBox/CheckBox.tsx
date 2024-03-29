dbce5b71fcb6c1824a739d0983c5bd803e121ca7
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2024-03-29 11:58:02 +0300

    Checkbox component

---
+++ src/shared/ui/redesigned/popups/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,54 @@
+import React, { ReactNode, useState } from 'react';
+
+import { classNames } from '@/shared/lib/helpers/classNames/classNames';
+
+import cls from './CheckBox.module.scss';
+
+interface CheckboxItem<T extends string> {
+  value: T;
+  content: ReactNode;
+}
+
+interface CheckboxProps<T extends string> {
+  items: CheckboxItem<T>[];
+  className?: string;
+  values?: T[];
+  onChange?: (values: T[]) => void;
+  label?: string | null;
+}
+
+export function CheckBox<T extends string>(props: CheckboxProps<T>) {
+  const { className, items, values, onChange, label } = props;
+
+  const [checkedValues, setCheckedValues] = useState(values || []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const triggeredItem = items?.find((item) => item.value === event.target.value)?.value;
+    const newValues =
+      event.target.checked && triggeredItem
+        ? [...checkedValues, triggeredItem]
+        : checkedValues.filter((value) => value !== event.target.value);
+    setCheckedValues(newValues);
+    onChange?.(newValues);
+  };
+
+  return (
+    <fieldset className={classNames(cls.checkbox, {}, [className])}>
+      {label && <legend>{`${label}`}</legend>}
+      {items.map((item) => (
+        <div key={item.value}>
+          <label htmlFor={item.value}>
+            <input
+              type="checkbox"
+              id={item.value}
+              value={item.value}
+              checked={checkedValues.includes(item.value)}
+              onChange={handleChange}
+            />
+            {item.content}
+          </label>
+        </div>
+      ))}
+    </fieldset>
+  );
+}
